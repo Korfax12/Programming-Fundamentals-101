acc02541690a13981898875e2456276f6dca34e5
dzhuneyt <dzhuneyt@dzhuneyt.com>
2019-12-04 21:30:08 +0200

    chore: Example command on how to start local MySQL feat: REST endpoints for creating and reading chat messages

--- examples/chat/routes/api.php
+++ examples/chat/routes/api.php
@@ -1,18 +1,11 @@
 <?php
+Route::get('message', function(){
+    return App\Chat::all();
+});
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('create_message', function(){
+    $row = new App\Chat();
+    $row->message = 'Something cool';
+    $row->author = 'Johnny';
+    $row->save();
 });
