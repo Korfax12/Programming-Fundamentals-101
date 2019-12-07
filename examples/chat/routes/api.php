42c901e84f66c7f913d2e8813dad98c28b2d6b13
dzhuneyt <dzhuneyt@dzhuneyt.com>
2019-12-07 11:17:37 +0200

    CRUD APIs for chat messages Updated command to start MySQL

--- examples/chat/routes/api.php
+++ examples/chat/routes/api.php
@@ -1,11 +1,31 @@
 <?php
+use Illuminate\Http\Request;
+
 Route::get('message', function(){
     return App\Chat::all();
 });
 
-Route::get('create_message', function(){
+Route::post('message', function(Request $request){
+    $author = $request->get('author');
+    $messageItself = $request->get('message');
+
     $row = new App\Chat();
-    $row->message = 'Something cool';
-    $row->author = 'Johnny';
+    $row->author = $author;
+    $row->message = $messageItself;
     $row->save();
+    return $row;
+});
+
+Route::put('message/{id}', function(string $id, Request $request){
+    $messageItself = $request->get('message');
+    $model = App\Chat::findOrFail($id);
+    $model->message = $messageItself;
+    $model->save();
+    return $model;
+});
+
+Route::delete('message/{id}', function(string $id){
+    $model = App\Chat::findOrFail($id);
+    $model->delete();
+    return $model;
 });
